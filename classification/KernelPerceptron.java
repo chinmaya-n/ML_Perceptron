f44cda42ea80517c9701d6c8f826578193dc744f
unknown <inblueswithu@yahoo.co.in>
2015-03-29 23:26:03 -0400

    unsaved changes from last commit

--- classification/KernelPerceptron.java
+++ classification/KernelPerceptron.java
@@ -432,7 +432,7 @@ public class KernelPerceptron {
 	 * @return 1 - if 0 or +ve / -1 if -ve
 	 */
 	private int sign(double value) {
-		if(value > 0)
+		if(value >= 0)
 			return 1;
 		else
 			return -1;
