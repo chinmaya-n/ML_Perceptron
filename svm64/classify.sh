769aba62d5adcad10cf4786d88e947820566a3b7
unknown <inblueswithu@yahoo.co.in>
2015-03-30 21:42:06 -0400

    updated SVM code

--- svm64/classify.sh
+++ svm64/classify.sh
@@ -6,8 +6,12 @@ cd ~/Documents/svm64
 for i in {0..9}
 do
 	# classify linear egs
-	./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_linear.model ./results/${i}_linear_svm.result
+	#./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_linear.model ./results/${i}_linear_svm.result
+
+	# classify polynomial egs
+#	./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_polynomial5.model ./results/${i}_polynomial5_svm.result
 
 	# classify gaussians egs
-    #./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_gaus_gamma0.5.model ./results/${i}_svm.result
+    	./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_gaus_gamma0.005.model ./results/${i}_svm_gaus_gamma0.005.result
+#	./svm_classify ./data/Digit${i}_svm.tes ./models/${i}_gaus_gamma2.model ./results/${i}_svm_gaus_gamma2.result
 done
--- svm64/training.sh
+++ svm64/training.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-cd ~/Documents/svm64
+#cd ~/Documents/svm64
 
 # generate models for training files
 for i in {0..9}
 do
 	# linear
-    ./svm_learn ./data/Digit${i}_svm.tra ./models/${i}_linear.model
+#	./svm_learn ./data/Digit${i}_svm.tra ./models/${i}_linear.model
+
+	# polynomial
+#	./svm_learn -t 1 -d 5 -c 1 ./data/Digit${i}_svm.tra ./models/${i}_polynomial5.model
 
 	# gaussian: gamma = 0.005;
-	#./svm_learn -g 0.005 -t 2 ./data/Digit${i}_svm.tra ./models/${i}_gaus_gamma0.5.model
+	./svm_learn -g 0.005 -t 2 ./data/Digit${i}_svm.tra ./models/${i}_gaus_gamma0.005.model
+#	./svm_learn -g 2 -t 2 ./data/Digit${i}_svm.tra ./models/${i}_gaus_gamma2.model
 done
 
 
